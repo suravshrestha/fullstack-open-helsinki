a3e4f69c7e5cff6a762ed2c42a2a472f852f5b4d
Surav Shrestha <surav_02@live.com>
2022-10-04 17:03:27 +0545

    React - Effect hooks

--- part2/course/src/App.js
+++ part2/course/src/App.js
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Note from "./components/Note";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((res) => {
+      console.log("promise fulfilled");
+      setNotes(res.data);
+    });
+  };
+
+  // Empty array is passed to run the effect only once
+  useEffect(hook, []);
+
+  console.log("render", notes.length, "notes");
 
   const addNote = (event) => {
     event.preventDefault();
