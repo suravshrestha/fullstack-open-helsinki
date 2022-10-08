a185b1ab84bd8ff8d8cbc2d004130b3e8553ffc9
Surav Shrestha <surav_02@live.com>
2022-10-08 10:43:48 +0545

    Services

--- part2/course/src/App.js
+++ part2/course/src/App.js
@@ -1,22 +1,19 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+
 import Note from "./components/Note";
+import noteService from "./services/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
 
-  const hook = () => {
-    console.log("effect");
-    axios.get("http://localhost:3001/notes").then((res) => {
-      console.log("promise fulfilled");
-      setNotes(res.data);
-    });
-  };
-
   // Empty array is passed to run the effect only once
-  useEffect(hook, []);
+  useEffect(() => {
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
+  }, []);
 
   console.log("render", notes.length, "notes");
 
@@ -28,8 +25,8 @@ const App = () => {
       important: Math.random() < 0.5,
     };
 
-    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
-      setNotes(notes.concat(response.data));
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
       setNewNote("");
     });
   };
@@ -41,12 +38,11 @@ const App = () => {
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`;
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
 
-    axios.put(url, changedNote).then((res) => {
-      setNotes(notes.map((n) => (n.id === id ? res.data : n)));
+    noteService.update(id, changedNote).then((returnedNote) => {
+      setNotes(notes.map((note) => (note.id === id ? returnedNote : note)));
     });
   };
 
