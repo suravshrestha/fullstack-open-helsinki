5eb584d71c832b9a1002670fa2c2b767a78ea79b
Surav Shrestha <surav_02@live.com>
2022-10-10 10:27:31 +0545

    Express: Fetch a single resource

--- part03/course/index.js
+++ part03/course/index.js
@@ -30,6 +30,17 @@ app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
+app.get("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    return res.json(note);
+  }
+
+  res.status(404).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
