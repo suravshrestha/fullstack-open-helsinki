0ac95faa8e4f377c8017c5e99153f13f179d48b8
Surav Shrestha <surav_02@live.com>
2022-10-10 09:24:55 +0545

    Express: Simple web server

--- part03/course/index.js
+++ part03/course/index.js
@@ -1,4 +1,5 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
 let notes = [
   {
@@ -21,11 +22,15 @@ let notes = [
   },
 ];
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(notes));
+app.get("/", (req, res) => {
+  res.send("<h1>Hello, world!</h1>");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
 });
 
 const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
