382bc24012a72dd2714d52d01f9c40e442d2981b
Surav Shrestha <surav_02@live.com>
2022-10-17 17:52:07 +0545

    Testing: Initialize the database before each test

--- part-04-testing-express-servers-and-user-administration/course/tests/note_api.test.js
+++ part-04-testing-express-servers-and-user-administration/course/tests/note_api.test.js
@@ -5,6 +5,31 @@ const app = require("../app");
 // superagent object
 const api = supertest(app);
 
+const Note = require("../models/note");
+
+const initialNotes = [
+  {
+    content: "HTML is easy",
+    date: new Date(),
+    important: false,
+  },
+  {
+    content: "Browser can execute only Javascript",
+    date: new Date(),
+    important: true,
+  },
+];
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+
+  noteObject = new Note(initialNotes[1]);
+  await noteObject.save();
+});
+
 test("notes are returned as json", async () => {
   await api
     .get("/api/notes")
@@ -12,18 +37,25 @@ test("notes are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 });
 
-test("there are two notes", async () => {
+test("all notes are returned", async () => {
   const response = await api.get("/api/notes");
 
   // execution gets here only after the HTTP request is complete
   // the result of HTTP request is saved in variable response
-  expect(response.body).toHaveLength(2);
+  expect(response.body).toHaveLength(initialNotes.length);
 });
 
 test("the first note is about HTTP methods", async () => {
   const response = await api.get("/api/notes");
 
-  expect(response.body[0].content).toBe("HTML is Easy");
+  expect(response.body[0].content).toBe("HTML is easy");
+});
+
+test("a specific note is within the returned notes", async () => {
+  const response = await api.get("/api/notes");
+
+  const contents = response.body.map((r) => r.content);
+  expect(contents).toContain("Browser can execute only Javascript");
 });
 
 afterAll(() => {
