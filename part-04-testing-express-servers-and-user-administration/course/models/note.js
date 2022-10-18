09af1280610b0c3eabc054201ae97eacb17a77c0
Surav Shrestha <surav_02@live.com>
2022-10-18 18:14:20 +0545

    User administration: User schema for users

--- part-04-testing-express-servers-and-user-administration/course/models/note.js
+++ part-04-testing-express-servers-and-user-administration/course/models/note.js
@@ -11,6 +11,10 @@ const noteSchema = new mongoose.Schema({
     required: true,
   },
   important: Boolean,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 // .toJSON method is called everytime res.send is called
