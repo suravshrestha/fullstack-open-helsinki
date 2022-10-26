4a62d1236c3981335bcfe4ce3b27604e91ef21f7
Surav Shrestha <surav_02@live.com>
2022-10-26 08:37:18 +0545

    User administration: Creating notes with API route

--- part-04-testing-express-servers-and-user-administration/course/controllers/notes.js
+++ part-04-testing-express-servers-and-user-administration/course/controllers/notes.js
@@ -1,5 +1,7 @@
 const notesRouter = require("express").Router();
+
 const Note = require("../models/note");
+const User = require("../models/user");
 
 notesRouter.get("/", async (req, res) => {
   const notes = await Note.find({});
@@ -20,13 +22,18 @@ notesRouter.get("/:id", async (req, res) => {
 notesRouter.post("/", async (req, res) => {
   const body = req.body;
 
+  const user = await User.findById(body.userId);
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
+    user: user._id,
   });
 
   const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
 
   res.status(201).json(savedNote);
 });
