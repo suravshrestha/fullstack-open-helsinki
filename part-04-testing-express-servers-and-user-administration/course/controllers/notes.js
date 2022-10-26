946cb80968b459532d65fa766825e96dc876247b
Surav Shrestha <surav_02@live.com>
2022-10-26 10:13:45 +0545

    User administration: Creating notes for logged-in users only

--- part-04-testing-express-servers-and-user-administration/course/controllers/notes.js
+++ part-04-testing-express-servers-and-user-administration/course/controllers/notes.js
@@ -1,7 +1,19 @@
+const jwt = require("jsonwebtoken");
+
 const notesRouter = require("express").Router();
 
 const Note = require("../models/note");
 const User = require("../models/user");
+
+const getTokenFrom = (req) => {
+  const authorization = req.get("authorization");
+
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+
+  return null;
+};
 
 notesRouter.get("/", async (req, res) => {
   // populate: Mongoose's join query
@@ -27,7 +39,14 @@ notesRouter.get("/:id", async (req, res) => {
 notesRouter.post("/", async (req, res) => {
   const body = req.body;
 
-  const user = await User.findById(body.userId);
+  const token = getTokenFrom(req);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+
+  if (!token || !decodedToken.id) {
+    return res.status(401).json({ error: "token missing or invalid" });
+  }
+
+  const user = await User.findById(decodedToken.id);
 
   const note = new Note({
     content: body.content,
