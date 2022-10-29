0f6d89a6ec74feed253cd6879399233884dffd2e
Surav Shrestha <surav_02@live.com>
2022-10-29 13:36:32 +0545

    E2E Testing: Testing new note form

--- part-05-testing-react-apps/course/frontend/cypress/e2e/note_app.cy.js
+++ part-05-testing-react-apps/course/frontend/cypress/e2e/note_app.cy.js
@@ -22,4 +22,20 @@ describe("Note app", function () {
 
     cy.contains("Janet Doe logged in");
   });
+
+  describe("when logged in", function () {
+    beforeEach(function () {
+      cy.contains("login").click();
+      cy.get("input:first").type("janetdoe");
+      cy.get("input:last").type("ilovejanetdoe");
+      cy.get("#login-button").click();
+    });
+
+    it("a new note can be created", function () {
+      cy.contains("button", "new note").click();
+      cy.get("#note-input").type("a note created by cypress");
+      cy.contains("button", "save").click();
+      cy.contains("a note created by cypress");
+    });
+  });
 });
