0576adb87a488f0403606b3ed0f799201797e68b
Surav Shrestha <surav_02@live.com>
2022-10-29 17:03:43 +0545

    E2E Testing: Changing importance of notes

--- part-05-testing-react-apps/course/frontend/cypress/e2e/note_app.cy.js
+++ part-05-testing-react-apps/course/frontend/cypress/e2e/note_app.cy.js
@@ -76,14 +76,29 @@ describe("Note app", function () {
       });
 
       it("it can be made not important", function () {
+        // .as: assign an alias to the button
         cy.contains("another note cypress")
-          .contains("button", "make not important")
-          .click();
+          .parent()
+          .find("button")
+          .contains("make not important")
+          .as("theButton");
 
-        cy.contains("another note cypress").contains(
-          "button",
-          "make not important"
-        );
+        cy.get("@theButton").click();
+        cy.get("@theButton").contains("make not important");
+      });
+    });
+
+    describe("and several notes exist", function () {
+      beforeEach(function () {
+        cy.createNote({ content: "first note", important: false });
+        cy.createNote({ content: "second note", important: false });
+        cy.createNote({ content: "third note", important: false });
+      });
+
+      it("one of those can be made important", function () {
+        cy.contains("second note").parent().find("button").as("theButton");
+        cy.get("@theButton").click();
+        cy.get("@theButton").should("contain", "make not important");
       });
     });
   });
--- part-05-testing-react-apps/course/frontend/src/components/Note.js
+++ part-05-testing-react-apps/course/frontend/src/components/Note.js
@@ -6,7 +6,7 @@ const Note = ({ note, toggleImportance }) => {
 
   return (
     <li className="note">
-      Your awesome note: {note.content}
+      <span>Your awesome note: {note.content}</span>{" "}
       <button onClick={toggleImportance}>{label}</button>
     </li>
   );
