673667d1dbe0730adb90453daaf57a8cd6cc64cf
Surav Shrestha <surav_02@live.com>
2022-10-29 13:18:07 +0545

    E2E Testing: Form testing

--- part-05-testing-react-apps/course/frontend/cypress/e2e/note_app.cy.js
+++ part-05-testing-react-apps/course/frontend/cypress/e2e/note_app.cy.js
@@ -1,9 +1,25 @@
 describe("Note app", function () {
-  it("front page can be opened", function () {
+  beforeEach(function () {
     cy.visit("http://localhost:3000");
+  });
+
+  it("front page can be opened", function () {
     cy.contains("Notes");
     cy.contains(
       "Note app, Department of Computer Science, University of Helsinki 2022"
     );
+  });
+
+  it("login form can be opened", function () {
+    cy.contains("login").click();
+  });
+
+  it("user can log in", function () {
+    cy.contains("login").click();
+    cy.get("#username").type("janetdoe");
+    cy.get("#password").type("ilovejanetdoe");
+    cy.get("#login-button").click();
+
+    cy.contains("Janet Doe logged in");
   });
 });
