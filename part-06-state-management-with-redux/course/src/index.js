6a419148627f62c3ae2368150dd1ca594e95b05c
Surav Shrestha <surav_02@live.com>
2022-10-30 16:10:38 +0545

    Uncontrolled form in react using Redux

--- part-06-state-management-with-redux/course/src/index.js
+++ part-06-state-management-with-redux/course/src/index.js
@@ -4,57 +4,48 @@ import ReactDOM from "react-dom/client";
 import { createStore } from "redux";
 
 import noteReducer from "./reducers/noteReducer";
-import counterReducer from "./reducers/counterReducer";
 
 // Create a Redux store holding the state of the app.
 // Its API is { subscribe, dispatch, getState }.
-const noteStore = createStore(noteReducer);
+const store = createStore(noteReducer);
 
-const counterStore = createStore(counterReducer);
-
-noteStore.dispatch({
-  type: "NEW_NOTE",
-  data: {
-    content: "the app state is in redux store",
-    important: true,
-    id: 1,
-  },
-});
-
-noteStore.dispatch({
-  type: "NEW_NOTE",
-  data: {
-    content: "state changes are made with actions",
-    important: false,
-    id: 2,
-  },
-});
+const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
 const App = () => {
+  const addNote = (event) => {
+    event.preventDefault();
+    const content = event.target.note.value; // <input name="note" />
+    event.target.note.value = "";
+    store.dispatch({
+      type: "NEW_NOTE",
+      data: {
+        content,
+        important: false,
+        id: generateId(),
+      },
+    });
+  };
+
+  const toggleImportance = (id) => {
+    store.dispatch({
+      type: "TOGGLE_IMPORTANCE",
+      data: { id },
+    });
+  };
+
   return (
     <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
       <ul>
-        {noteStore.getState().map((note) => (
-          <li key={note.id}>
+        {store.getState().map((note) => (
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
             {note.content} <strong>{note.important ? "important" : ""}</strong>
           </li>
         ))}
       </ul>
-
-      <div>{counterStore.getState()}</div>
-
-      <div>
-        {/* Mutate the internal state using dispatch */}
-        <button onClick={(e) => counterStore.dispatch({ type: "INCREMENT" })}>
-          plus
-        </button>
-        <button onClick={(e) => counterStore.dispatch({ type: "DECREMENT" })}>
-          minus
-        </button>
-        <button onClick={(e) => counterStore.dispatch({ type: "ZERO" })}>
-          zero
-        </button>
-      </div>
     </div>
   );
 };
@@ -66,5 +57,4 @@ renderApp();
 
 // store calls the callback function(renderApp) whenever
 // an action is dispatched to the store.
-noteStore.subscribe(renderApp);
-counterStore.subscribe(renderApp);
+store.subscribe(renderApp);
