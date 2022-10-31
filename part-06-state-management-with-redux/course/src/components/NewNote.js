f1b293d41c7d94ff04a5e2ccbfbd1b23356ba1f5
Surav Shrestha <surav_02@live.com>
2022-10-31 16:03:53 +0545

    react-redux legacy connect function

--- part-06-state-management-with-redux/course/src/components/NewNote.js
+++ part-06-state-management-with-redux/course/src/components/NewNote.js
@@ -1,15 +1,12 @@
-import { useDispatch } from "react-redux";
-
+import { connect } from "react-redux";
 import { createNote } from "../reducers/noteReducer";
 
-const NewNote = () => {
-  const dispatch = useDispatch();
-
+const NewNote = (props) => {
   const addNote = (event) => {
     event.preventDefault();
     const content = event.target.note.value;
     event.target.note.value = "";
-    dispatch(createNote(content));
+    props.createNote(content);
   };
 
   return (
@@ -20,4 +17,4 @@ const NewNote = () => {
   );
 };
 
-export default NewNote;
+export default connect(null, { createNote })(NewNote);
