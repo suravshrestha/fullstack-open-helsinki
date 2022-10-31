26671edae0626c17c29c98c03257e6215edce3a7
Surav Shrestha <surav_02@live.com>
2022-10-31 19:57:08 +0545

    React router: useMatch

--- part-07-react-routers,custom-hooks,ui-frameworks,webpack/course/src/App.js
+++ part-07-react-routers,custom-hooks,ui-frameworks,webpack/course/src/App.js
@@ -1,13 +1,12 @@
 import { useState } from "react";
 
 import {
-  BrowserRouter as Router,
   Routes,
   Route,
   Link,
   Navigate,
-  useParams,
   useNavigate,
+  useMatch,
 } from "react-router-dom";
 
 const Home = () => (
@@ -27,9 +26,7 @@ const Home = () => (
   </div>
 );
 
-const Note = ({ notes }) => {
-  const id = useParams().id;
-  const note = notes.find((n) => n.id === Number(id));
+const Note = ({ note }) => {
   return (
     <div>
       <h2>{note.content}</h2>
@@ -122,41 +119,44 @@ const App = () => {
     padding: 5,
   };
 
+  const match = useMatch("/notes/:id");
+  const note = match
+    ? notes.find((note) => note.id === Number(match.params.id))
+    : null;
+
   return (
     <div>
-      <Router>
-        <div>
-          <Link style={padding} to="/">
-            home
+      <div>
+        <Link style={padding} to="/">
+          home
+        </Link>
+        <Link style={padding} to="/notes">
+          notes
+        </Link>
+        <Link style={padding} to="/users">
+          users
+        </Link>
+        {user ? (
+          <em>{user} logged in</em>
+        ) : (
+          <Link style={padding} to="/login">
+            login
           </Link>
-          <Link style={padding} to="/notes">
-            notes
-          </Link>
-          <Link style={padding} to="/users">
-            users
-          </Link>
-          {user ? (
-            <em>{user} logged in</em>
-          ) : (
-            <Link style={padding} to="/login">
-              login
-            </Link>
-          )}
-        </div>
+        )}
+      </div>
 
-        <Routes>
-          <Route path="/notes/:id" element={<Note notes={notes} />} />
-          <Route path="/notes" element={<Notes notes={notes} />} />
-          <Route
-            path="/users"
-            // If the user is not logged in, the user is redirected to the login view
-            // using the Navigate component
-            element={user ? <Users /> : <Navigate replace to="/login" />}
-          />
-          <Route path="/login" element={<Login onLogin={login} />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/notes/:id" element={<Note note={note} />} />
+        <Route path="/notes" element={<Notes notes={notes} />} />
+        <Route
+          path="/users"
+          // If the user is not logged in, the user is redirected to the login view
+          // using the Navigate component
+          element={user ? <Users /> : <Navigate replace to="/login" />}
+        />
+        <Route path="/login" element={<Login onLogin={login} />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
 
       <footer>
         <em>Note app, Department of Computer Science 2022</em>
