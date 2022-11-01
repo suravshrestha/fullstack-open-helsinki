ce92576f95f9f054ad8244cabb8254393bfb3a56
Surav Shrestha <surav_02@live.com>
2022-11-01 11:27:10 +0545

    React Bootstrap: Forms

--- part-07-react-routers,custom-hooks,ui-frameworks,webpack/course/src/App.js
+++ part-07-react-routers,custom-hooks,ui-frameworks,webpack/course/src/App.js
@@ -9,7 +9,7 @@ import {
   useMatch,
 } from "react-router-dom";
 
-import { Table } from "react-bootstrap";
+import { Table, Form, Button } from "react-bootstrap";
 
 const Home = () => (
   <div>
@@ -81,15 +81,17 @@ const Login = (props) => {
   return (
     <div>
       <h2>login</h2>
-      <form onSubmit={onSubmit}>
-        <div>
-          username: <input />
-        </div>
-        <div>
-          password: <input type="password" />
-        </div>
-        <button type="submit">login</button>
-      </form>
+      <Form onSubmit={onSubmit}>
+        <Form.Group>
+          <Form.Label>username:</Form.Label>
+          <Form.Control type="text" name="username" />
+          <Form.Label>password:</Form.Label>
+          <Form.Control type="password" />
+          <Button variant="primary" type="submit">
+            login
+          </Button>
+        </Form.Group>
+      </Form>
     </div>
   );
 };
