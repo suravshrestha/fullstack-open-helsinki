d210ed847af235e93ce7ae99df63e16e857bbf9f
Surav Shrestha <surav_02@live.com>
2022-11-01 11:26:46 +0545

    React Bootstrap

--- part-07-react-routers,custom-hooks,ui-frameworks,webpack/course/src/App.js
+++ part-07-react-routers,custom-hooks,ui-frameworks,webpack/course/src/App.js
@@ -9,6 +9,8 @@ import {
   useMatch,
 } from "react-router-dom";
 
+import { Table } from "react-bootstrap";
+
 const Home = () => (
   <div>
     <h2>TKTL notes app</h2>
@@ -41,13 +43,18 @@ const Note = ({ note }) => {
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <ul>
-      {notes.map((note) => (
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      ))}
-    </ul>
+    <Table striped>
+      <tbody>
+        {notes.map((note) => (
+          <tr key={note.id}>
+            <td>
+              <Link to={`/notes/${note.id}`}>{note.content}</Link>
+            </td>
+            <td>{note.user}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   </div>
 );
 
@@ -125,7 +132,7 @@ const App = () => {
     : null;
 
   return (
-    <div>
+    <div className="container">
       <div>
         <Link style={padding} to="/">
           home
