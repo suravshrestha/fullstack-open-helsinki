1477b1af222c5a9f055f585f682ddc02ca7fb838
Surav Shrestha <surav_02@live.com>
2023-02-19 21:52:56 +0545

    GraphQL enum

--- part-08-graphql/course/index.js
+++ part-08-graphql/course/index.js
@@ -38,9 +38,14 @@ const typeDefs = `
     id: ID!
   }
 
+  enum YesNo {
+    YES
+    NO
+  }
+
   type Query {
     personCount: Int!
-    allPersons: [Person!]!
+    allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
   }
 
@@ -59,7 +64,15 @@ const { v1: uuid } = require("uuid");
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+      if (!args.phone) {
+        return persons;
+      }
+
+      const byPhone = (person) =>
+        args.phone === "YES" ? person.phone : !person.phone;
+      return persons.filter(byPhone);
+    },
     findPerson: (root, args) => persons.find((p) => p.name === args.name),
   },
 
