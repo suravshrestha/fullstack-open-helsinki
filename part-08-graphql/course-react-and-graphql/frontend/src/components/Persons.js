c49f2c12e9b92e24568f9802094c3036fe0e61b5
Surav Shrestha <surav_02@live.com>
2023-02-21 13:30:58 +0545

    React and GraphQL: Named queries and variables

--- part-08-graphql/course-react-and-graphql/frontend/src/components/Persons.js
+++ part-08-graphql/course-react-and-graphql/frontend/src/components/Persons.js
@@ -1,10 +1,56 @@
+import { useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const FIND_PERSON = gql`
+  query findPersonByName($nameToSearch: String!) {
+    findPerson(name: $nameToSearch) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`;
+
+const Person = ({ person, onClose }) => {
+  return (
+    <div>
+      <h2>{person.name}</h2>
+      <div>
+        {person.address.street} {person.address.city}
+      </div>
+      <div>{person.phone}</div>
+      <button onClick={onClose}>close</button>
+    </div>
+  );
+};
+
 const Persons = ({ persons }) => {
+  const [nameToSearch, setNameToSearch] = useState(null);
+  const result = useQuery(FIND_PERSON, {
+    variables: { nameToSearch },
+    skip: !nameToSearch, // skip the query if nameToSearch is null
+  });
+
+  if (nameToSearch && result.data) {
+    return (
+      <Person
+        person={result.data.findPerson}
+        onClose={() => setNameToSearch(null)}
+      />
+    );
+  }
+
   return (
     <div>
       <h2>Persons</h2>
       {persons.map((p) => (
         <div key={p.name}>
-          {p.name} {p.phone}
+          {p.name} {p.phone}{" "}
+          <button onClick={() => setNameToSearch(p.name)}>show address</button>
         </div>
       ))}
     </div>
