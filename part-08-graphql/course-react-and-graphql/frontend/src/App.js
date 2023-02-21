b11c9f4abcba1975268f1953334598937462dc02
Surav Shrestha <surav_02@live.com>
2023-02-21 13:22:34 +0545

    React and GraphQL: Making queries

--- part-08-graphql/course-react-and-graphql/frontend/src/App.js
+++ part-08-graphql/course-react-and-graphql/frontend/src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <div>Hello, world!</div>;
-}
+import Persons from "./components/Persons";
+
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
+  }
+`;
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS);
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+
+  return <Persons persons={result.data.allPersons} />;
+};
 
 export default App;
