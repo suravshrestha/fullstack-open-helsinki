591b3522448b1e7ad22dc16ae33503d1e311ddee
Surav Shrestha <surav.shrestha@outlook.com>
2024-10-24 17:19:44 +0545

    GraphQL - DB and user administration: Friends list

--- part-08-graphql/course-react-and-graphql/backend/index.js
+++ part-08-graphql/course-react-and-graphql/backend/index.js
@@ -81,6 +81,10 @@ const typeDefs = `
       username: String!
       password: String!
     ): Token
+
+    addAsFriend(
+      name: String!
+    ): User
   }
 `;
 
@@ -113,14 +117,25 @@ const resolvers = {
   },
 
   Mutation: {
-    addPerson: async (root, args) => {
+    addPerson: async (root, args, context) => {
       const person = new Person({ ...args });
+      const currentUser = context.currentUser;
+
+      if (!currentUser) {
+        throw new GraphQLError("not authenticated", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+          },
+        });
+      }
 
       try {
         await person.save();
+        currentUser.friends = currentUser.friends.concat(person);
+        await currentUser.save();
       } catch (error) {
-        // GraphQl Validation
-        throw new GraphQLError("Saving person failed", {
+        // GraphQl error handling
+        throw new GraphQLError("Saving user failed", {
           extensions: {
             code: "BAD_USER_INPUT",
             invalidArgs: args.name,
@@ -130,6 +145,29 @@ const resolvers = {
       }
 
       return person;
+    },
+
+    // Destructure the context object to get the currentUser
+    addAsFriend: async (root, args, { currentUser }) => {
+      const isFriend = (person) =>
+        currentUser.friends
+          .map((f) => f._id.toString())
+          .includes(person._id.toString());
+
+      if (!currentUser) {
+        throw new GraphQLError("wrong credentials", {
+          extensions: { code: "BAD_USER_INPUT" },
+        });
+      }
+
+      const person = await Person.findOne({ name: args.name });
+      if (!isFriend(person)) {
+        currentUser.friends = currentUser.friends.concat(person);
+      }
+
+      await currentUser.save();
+
+      return currentUser;
     },
 
     editNumber: async (root, args) => {
