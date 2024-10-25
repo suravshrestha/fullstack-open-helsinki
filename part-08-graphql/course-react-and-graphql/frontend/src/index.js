1fe1ba9742b15a4bf8950217dfe30f15fd7a4e3d
Surav Shrestha <surav.shrestha@outlook.com>
2024-10-25 15:45:54 +0545

    GraphQL frontend: user login, add token to header

--- part-08-graphql/course-react-and-graphql/frontend/src/index.js
+++ part-08-graphql/course-react-and-graphql/frontend/src/index.js
@@ -5,12 +5,31 @@ import {
   ApolloClient,
   ApolloProvider,
   InMemoryCache,
+  createHttpLink,
   gql,
 } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("phonenumbers-user-token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000",
+});
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
   cache: new InMemoryCache(),
+
+  // The link is modified by the context defined by the authLink object so that a possible token\
+  // in localStorage is set to header authorization for each request to the server.
+  link: authLink.concat(httpLink),
 });
 
 const query = gql`
@@ -34,5 +53,5 @@ client.query({ query }).then((response) => {
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ApolloProvider client={client}>
     <App />
-  </ApolloProvider>
+  </ApolloProvider>,
 );
