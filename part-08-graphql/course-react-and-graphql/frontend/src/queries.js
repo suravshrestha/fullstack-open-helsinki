bc01b98819bd4981bc24f664fe6742b596f10790
Surav Shrestha <surav.shrestha@outlook.com>
2024-10-26 20:49:23 +0545

    GraphQL: Fragments

--- part-08-graphql/course-react-and-graphql/frontend/src/queries.js
+++ part-08-graphql/course-react-and-graphql/frontend/src/queries.js
@@ -1,27 +1,67 @@
 import { gql } from "@apollo/client";
 
+/* It is pretty common in GraphQL that multiple queries return similar results.
+
+For example, the query for the details of a person
+
+query {
+  findPerson(name: "Pekka Mikkola") {
+    name
+    phone
+    address{
+      street 
+      city
+    }
+  }
+}
+and the query for all persons
+
+query {
+  allPersons {
+    name
+    phone
+    address{
+      street 
+      city
+    }
+  }
+}
+both return persons.
+
+When choosing the fields to return, both queries have to define exactly the same fields.
+
+These kinds of situations can be simplified with the use of `fragments`. */
+
+const PERSON_DETAILS = gql`
+  fragment PersonDetails on Person {
+    id
+    name
+    phone
+    address {
+      street
+      city
+    }
+  }
+`;
+
+// The fragment can be placed to any query or mutation using a dollar sign and curly braces:
+// With the fragment, we can do the queries in a compact form:
 export const ALL_PERSONS = gql`
   query {
     allPersons {
-      name
-      phone
-      id
+      ...PersonDetails
     }
   }
+  ${PERSON_DETAILS}
 `;
 
 export const FIND_PERSON = gql`
   query findPersonByName($nameToSearch: String!) {
     findPerson(name: $nameToSearch) {
-      name
-      phone
-      id
-      address {
-        street
-        city
-      }
+      ...PersonDetails
     }
   }
+  ${PERSON_DETAILS}
 `;
 
 export const CREATE_PERSON = gql`
